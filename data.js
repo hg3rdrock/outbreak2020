17865b49d69eb88a2106a8934c71358e40012f18
Hu Gang <gone.who@gmail.com>
2020-01-26 09:45:34 +0800

    update multiple cities

--- data.js
+++ data.js
@@ -2,13 +2,13 @@
         "name": "湖北",
         "lng": 114.284878,
         "lat": 30.666894,
-        "num_total": 788
+        "num_total": 1052
       },
       {
         "name": "广东",
         "lng": 113.266530,
         "lat": 23.132191,
-        "num_total": 78
+        "num_total": 98
       },
       {
         "name": "北京",
@@ -20,7 +20,7 @@
         "name": "浙江",
         "lng": 120.16317,
         "lat": 30.23375,
-        "num_total": 62
+        "num_total": 104
       },
       {
         "name": "天津",
@@ -32,19 +32,19 @@
         "name": "上海",
         "lng": 121.47,
         "lat": 31.23,
-        "num_total": 33
+        "num_total": 40
       },
       {
         "name": "重庆",
         "lng": 106.551556,
         "lat": 29.563009,
-        "num_total": 57
+        "num_total": 75
       },
       {
         "name": "河南",
         "lng": 113.753602,
         "lat": 34.765515,
-        "num_total": 32
+        "num_total": 83
       },
       {
         "name": "四川",
@@ -62,19 +62,19 @@
         "name": "湖南",
         "lng": 112.983810,
         "lat": 28.112444,
-        "num_total": 43
+        "num_total": 69
       },
       {
         "name": "山东",
         "lng": 117.020359,
         "lat": 36.668530,
-        "num_total": 27
+        "num_total": 39
       },
       {
         "name": "山西",
         "lng": 112.562398,
         "lat": 37.873531,
-        "num_total": 6
+        "num_total": 9
       },
       {
         "name": "河北",
@@ -98,13 +98,13 @@
         "name": "甘肃",
         "lng": 103.826308,
         "lat": 36.059421,
-        "num_total": 4
+        "num_total": 7
       },
       {
         "name": "新疆",
         "lng": 87.627704,
         "lat": 43.793026,
-        "num_total": 3
+        "num_total": 4
       },
       {
         "name": "广西",
@@ -152,7 +152,7 @@
         "name": "黑龙江",
         "lng": 126.661669,
         "lat": 45.742347,
-        "num_total": 9
+        "num_total": 15
       },
       {
         "name": "吉林",
@@ -164,7 +164,7 @@
         "name": "江苏",
         "lng": 118.763232,
         "lat": 32.061707,
-        "num_total": 18
+        "num_total": 31
       },
       {
         "name": "陕西",
