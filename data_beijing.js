474b221375ec294f04fe581adbb3cf37c2a86421
Hu Gang <gone.who@gmail.com>
2020-02-03 23:32:29 +0800

    add Beijing data

---
+++ data_beijing.js
@@ -0,0 +1,84 @@
+var data = [
+  {
+    name: '天瑞嘉园',
+    lng: 116.205846,
+    lat: 39.68406,
+    confirmed: true
+  },
+  {
+    name: '华腾园',
+    lng: 116.465157,
+    lat: 39.887327,
+    confirmed: true
+  },
+  {
+    name: '观景园',
+    lng: 116.374345,
+    lat: 40.035476,
+    confirmed: true
+  },
+  {
+    name: '玉璟园',
+    lng: 116.454996,
+    lat: 39.764754,
+    confirmed: true
+  },
+  {
+    name: '北苑一号院',
+    lng: 116.427474,
+    lat: 40.023488,
+    confirmed: true
+  },
+  {
+    name: '林萃路10号院',
+    lng: 116.374133,
+    lat: 40.010469,
+    confirmed: true
+  },
+  {
+    name: '崇文门东大街6号楼',
+    lng: 116.429856,
+    lat: 39.900348,
+    confirmed: true
+  },
+  {
+    name: '西二旗空研大院',
+    lng: 116.321579,
+    lat: 40.055039,
+    confirmed: true
+  },
+  {
+    name: '怡锦园',
+    lng: 116.290355,
+    lat: 39.833529,
+    confirmed: true
+  }, {
+    name: '远洋自然',
+    lng: 116.390164,
+    lat: 39.840929,
+    confirmed: true
+  }, {
+    name: '龙锦苑',
+    lng: 116.368800,
+    lat: 40.085868,
+    confirmed: true
+  },
+  {
+    name: '澳林春天',
+    lng: 116.3738,
+    lat: 40.01226,
+    confirmed: true
+  },
+  {
+    name: '百旺茉莉园',
+    lng: 116.254795,
+    lat: 40.04819,
+    confirmed: true
+  },
+  {
+    name: '万年花城',
+    lng: 116.321640,
+    lat: 39.843816,
+    confirmed: true
+  },
+];
